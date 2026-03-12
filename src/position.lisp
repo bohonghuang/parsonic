a8aee6dccb7d0633351d306e727eab54f8b6f34d
Bohong Huang <bohonghuang@qq.com>
2026-03-12 11:11:07 +0800

    Allow seeking with parser `position`

--- src/position.lisp
+++ src/position.lisp
@@ -1,9 +1,13 @@
 (in-package #:parsonic)
 
 (defmethod expand-expr/eval ((op (eql 'position)) &rest args)
-  (destructuring-bind () args
-    '#'input-position/eval))
+  (destructuring-bind (&optional position) args
+    (if position
+        `(curry #'(setf input-position/eval) ,position)
+        '#'input-position/eval)))
 
 (defmethod expand-expr/compile ((op (eql 'position)) &rest args)
-  (destructuring-bind () args
-    (expand `(notinline (constantly ,(input-position/compile *codegen-input*))))))
+  (destructuring-bind (&optional position) args
+    (expand `(notinline (constantly ,(if position
+                                         (setf (input-position/compile *codegen-input*) position)
+                                         (input-position/compile *codegen-input*)))))))
