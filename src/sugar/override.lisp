8e8f276f56a16d977e7f0e35de9f3084ec175745
Bohong Huang <bohonghuang@qq.com>
2026-03-31 15:22:02 +0800

    Expand sugar arguments of primitive parsers in a single method

--- src/sugar/override.lisp
+++ src/sugar/override.lisp
@@ -1,15 +1,15 @@
 (in-package #:parsonic)
 
-(defmethod expand-expr :around ((op (eql 'satisfies)) &rest args)
-  (destructuring-bind (predicate) args
-    (if (and (consp predicate) (equal predicate `(curry #'eql ,(caddr predicate))))
-        (call-next-method)
-        (call-next-method
-         op (with-gensyms (result)
-              `(lambda (,result)
-                 (unless (eq ,result +input-eof+)
-                   (funcall ,predicate ,result))))))))
-
-(defmethod expand-expr :around ((op (eql 'rep)) &rest args)
-  (destructuring-bind (parser &optional (from 0) (to most-positive-fixnum)) args
-    (call-next-method op parser from to)))
+(defmethod expand-expr :around ((op symbol) &rest args)
+  (destructuring-case (cons op args)
+    ((satisfies predicate)
+     (if (and (consp predicate) (equal predicate `(curry #'eql ,(caddr predicate))))
+         (call-next-method)
+         (call-next-method
+          op (with-gensyms (result)
+               `(lambda (,result)
+                  (unless (eq ,result +input-eof+)
+                    (funcall ,predicate ,result)))))))
+    ((rep parser &optional (from 0) (to most-positive-fixnum))
+     (call-next-method op parser from to))
+    ((t &rest args) (declare (ignore args)) (call-next-method))))
