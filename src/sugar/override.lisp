ececc419d77c6a206ae1a5e30cb154d625bb1f57
Bohong Huang <bohonghuang@qq.com>
2026-04-12 19:40:12 +0800

    Special-case `(satisfies (constantly t))` for performance

--- src/sugar/override.lisp
+++ src/sugar/override.lisp
@@ -1,15 +1,20 @@
 (in-package #:parsonic)
 
+(declaim (inline not-eof-p))
+(defun not-eof-p (object)
+  (not (eq object +input-eof+)))
+
 (defmethod expand-expr :around ((op symbol) &rest args)
   (destructuring-case (cons op args)
     ((satisfies predicate)
      (if (and (consp predicate) (equal predicate `(curry #'eql ,(caddr predicate))))
          (call-next-method)
          (call-next-method
-          op (with-gensyms (result)
-               `(lambda (,result)
-                  (unless (eq ,result +input-eof+)
-                    (funcall ,predicate ,result)))))))
+          op (if (equal predicate '(constantly t))
+                 '#'not-eof-p
+                 (with-gensyms (result)
+                   `(lambda (,result)
+                      (and (not-eof-p ,result) (funcall ,predicate ,result))))))))
     ((rep parser &optional (from 0) (to most-positive-fixnum))
      (call-next-method op parser from to))
     ((t &rest args) (declare (ignore args)) (call-next-method))))
